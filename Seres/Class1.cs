e4078bdd4f7cd4522ee265ad84678f38f9a5e5ed
Antonio Dilone <antoniodilone26@gmail.com>
2023-06-05 23:18:22 -0400

    Agregar archivos de proyecto.

---
+++ Seres/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seres
+{
+    internal class Ser
+    {
+        public string nombre { get; set; }
+
+        public int edad { get; set; }
+
+        public double peso { get; set; }
+
+        public string tipo { get; set; }
+
+        public virtual void Respirar()
+        {
+            Console.WriteLine("El ser vivo esta respirando");
+        }
+        public virtual void Alimentarse() {
+            Console.WriteLine("El ser vivo se esta alimentando");
+        }
+    }
+
+    internal class humano : Ser {
+        public string cedula { get; set; }
+
+        public void Hablar() {
+            Console.WriteLine("El humano esta hablando");
+        }
+
+        public override void Alimentarse()
+        {
+            Console.WriteLine("El humano se esta alimentando");
+        }
+
+    }
+
+    internal class planta : Ser
+    {
+        public int petalos { get; set; }
+
+        public void Fotosintesis() {
+            Console.WriteLine("La planta hace fotosintesis");
+        }
+    }
+
+    internal class  animal : Ser {
+        public string tipoRepro { get; set; }
+
+        public void emitirSonido() {
+            Console.WriteLine("El animal emite un sonido");
+        }
+
+    }
+
+}
---
+++ Seres/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Specialized;
+
+namespace Seres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Ser> seres= new List<Ser>();
+            Console.WriteLine("Cuantos seres vivos desea agregar?");
+            int cantSeres = int.Parse(Console.ReadLine());  
+            Dictionary<string, int> humano= new Dictionary<string, int>();
+            Dictionary<string, int> planta= new Dictionary<string, int>();
+            Dictionary<string, int> animal= new Dictionary<string, int>();
+
+            for (int i = 0; i < cantSeres; i++)
+            {
+                Ser serVivo = new Ser();
+                Console.WriteLine("Cual es el nombre de este ser vivo");
+                serVivo.nombre = Console.ReadLine();
+                Console.WriteLine("Cual es la edad del ser vivo?");
+                serVivo.edad = int.Parse(Console.ReadLine());
+                Console.WriteLine("Cual es el peso del ser vivo?");
+                serVivo.peso = double.Parse(Console.ReadLine());
+                Console.WriteLine("Cual es el tipo de ser vivo? (Humano, animal, o planta)");
+                serVivo.tipo = Console.ReadLine().ToLower();
+                seres.Add(serVivo);
+                Console.Clear();
+            }
+            foreach (var item in seres)
+            {
+                if (humano.ContainsKey(item.tipo))
+                {
+                    humano[item.tipo]++;
+                } else if (animal.ContainsKey(item.tipo))
+                {
+                    animal[item.tipo]++;
+                }
+                else if (planta.ContainsKey(item.tipo))
+                {
+                    planta[item.tipo]++;
+                }
+                else
+                {
+                    humano[item.tipo] = 1;
+                    animal[item.tipo] = 1;
+                    planta[item.tipo] = 1;
+                }
+            }
+            
+            foreach (var item in seres.OrderByDescending(x => x.edad))
+            {
+                Console.WriteLine(item.nombre + " es un "+item.tipo+" de "+item.edad + " edad");
+            }
+            foreach (var item in seres)
+            {
+                Console.WriteLine(item.nombre + " es un " + item.tipo + " de " + item.edad + " edad");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
